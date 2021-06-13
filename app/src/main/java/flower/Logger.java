a1a9148a8c19e82948b7b5a962cfeaf05c0d19bd
Nabarun Mondal <nabarun.mondal@gmail.com>
2021-06-13 13:08:35 +0530

    Perf tests are added.

--- app/src/main/java/flower/Logger.java
+++ app/src/main/java/flower/Logger.java
@@ -7,18 +7,31 @@ public final class Logger {
     static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss:SSS" ;
     static final ThreadLocal<SimpleDateFormat> dateFormat = ThreadLocal.withInitial( ()-> new SimpleDateFormat(DATE_TIME_FORMAT));
 
+    private static boolean DISABLE = false;
+
+    public static synchronized void enable(){
+        DISABLE = false;
+    }
+
+    public static synchronized void disable(){
+        DISABLE = true;
+    }
+
     public static void info(String fmt, Object... args) {
+        if ( DISABLE ) return;
         final String res =  dateFormat.get().format(new Date()) + " | " + String.format(fmt, args);
         System.out.println(res);
     }
 
     public static void error(Throwable t, String fmt, Object... args) {
+        if ( DISABLE ) return;
         t.printStackTrace(System.err);
         final String res = dateFormat.get().format(new Date()) + " | " + String.format(fmt, args);
         System.err.println(res);
     }
 
     public static void warn(String fmt, Object... args) {
+        if ( DISABLE ) return;
         final String res = dateFormat.get().format(new Date()) + " | " + String.format(fmt, args);
         System.err.println(res);
     }
---
+++ app/src/test/java/flower/PerfTest.java
@@ -0,0 +1,69 @@
+package flower;
+
+import flower.workflow.DependencyWorkFlow;
+import flower.workflow.impl.MapDependencyWorkFlow;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import zoomba.lang.core.types.ZTypes;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+import static org.junit.Assert.assertNotNull;
+
+public class PerfTest {
+
+    static final double NANO_TO_MS = 0.000001 ;
+
+    @BeforeClass
+    public static void beforeClass(){
+        Logger.disable();
+    }
+
+    @AfterClass
+    public static void afterClass(){
+        Logger.enable();
+    }
+
+    static void testFile(final String path, final String node, final Map<String,Object> params, int times, double cutOfMS){
+        times = Math.max(times, 10);
+        List<Double> elapsedTime = new ArrayList<>();
+        for ( int i=0; i < times; i++) {
+            final long s = System.nanoTime();
+            DependencyWorkFlow workFlow = MapDependencyWorkFlow.MANAGER.load(path);
+            Map<String, Object> result = MapDependencyWorkFlow.MANAGER.run(workFlow, node, params);
+            assertNotNull(result);
+            final long e = System.nanoTime();
+            elapsedTime.add( (e-s) * NANO_TO_MS );
+        }
+        elapsedTime.sort((x,y) -> (int)Math.signum(x - y) );
+        int inx = (int)(times * 0.9);
+        double _9pc = elapsedTime.get(inx);
+        System.out.printf("Time Taken < %s > -> [%s] with %s (ns) 90%% : %.4f %n",
+                path, node, ZTypes.jsonString(params), _9pc);
+        Assert.assertTrue( "90% is slower than expected", cutOfMS > _9pc );
+    }
+
+    @Test
+    public void dummyWorkFlowTest() {
+        testFile( "samples/1.json", "c", Collections.emptyMap(), 100, 1.5 );
+    }
+
+    @Test
+    public void scriptingWorkFlowTest() {
+        // tiny work
+        Map<String, Object> params = new HashMap<>() {{
+            put("x", 1);
+            put("y", 2);
+        }};
+        testFile( "samples/op.yaml", "+", params, 500, 5.0 );
+        // more work
+        params = new HashMap<>() {{
+            put("x", new BigDecimal("12313132131321.1321312321313"));
+            put("y", new BigDecimal("12313132131321.1321312321313"));
+        }};
+        testFile( "samples/op.yaml", "+", params, 500, 5.0 );
+    }
+}
