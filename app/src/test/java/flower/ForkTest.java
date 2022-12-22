2e5e9225ea269fc251d33582545cc6172ba252da
Nabarun Mondal <nabarun.mondal@gmail.com>
2022-12-22 13:52:38 +0530

    Updating test cases.

--- app/src/test/java/flower/ForkTest.java
+++ app/src/test/java/flower/ForkTest.java
@@ -1,5 +1,6 @@
 package flower;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.*;
@@ -57,5 +58,41 @@ public class ForkTest extends MapWorkFlowTest {
         params.put("x", 100);
         Map<String,Object> result = testFile( "samples/fork/matches.yaml", rNode, params);
         assertEquals(false , result.get(STATUS));
+    }
+
+    @Test
+    public void testAllQuantifierOK(){
+        final String rNode = "all_over_x" ;
+        final Map<String,Object> params = new HashMap<>();
+        // gets all of them
+        params.put("x", 0);
+        Map<String,Object> result = testFile( "samples/fork/matches.yaml", rNode, params);
+        assertEquals(true , result.get(STATUS));
+        Object r = result.get(rNode);
+        assertTrue( r instanceof Map );
+        assertEquals( 3, ((Map<?, ?>) r).size());
+        // gets 2 of them
+        params.put("x", 15);
+        result = testFile( "samples/fork/matches.yaml", rNode, params);
+        assertEquals(true , result.get(STATUS));
+        r = result.get(rNode);
+        assertTrue( r instanceof Map );
+        assertEquals( 2, ((Map<?, ?>) r).size());
+
+        // gets only 1
+        params.put("x", 25);
+        result = testFile( "samples/fork/matches.yaml", rNode, params);
+        assertEquals(true , result.get(STATUS));
+        r = result.get(rNode);
+        assertTrue( r instanceof Map );
+        assertEquals( 1, ((Map<?, ?>) r).size());
+
+        // gets none
+        params.put("x", 100);
+        result = testFile( "samples/fork/matches.yaml", rNode, params);
+        assertEquals(true , result.get(STATUS));
+        r = result.get(rNode);
+        assertTrue( r instanceof Map );
+        assertEquals( 0, ((Map<?, ?>) r).size());
     }
 }
